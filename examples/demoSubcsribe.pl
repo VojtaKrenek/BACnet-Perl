5f894ef9f6c7e1defc722edb52abffa023916b5d
VojtaKrenek <167473580+VojtaKrenek@users.noreply.github.com>
2025-12-10 15:39:14 +0100

    feat: examples now takes arguments from comand line

--- examples/demoSubcsribe.pl
+++ examples/demoSubcsribe.pl
@@ -8,17 +8,27 @@ use constant TRUE  => 1;
 use constant FALSE => 0;
 
 use BACnet::Device;
-
 use Data::Dumper;
+
+# ---------------------------------------------------------------
+# Command-line arguments
+# ---------------------------------------------------------------
+if (@ARGV < 2) {
+    die "Usage: $0 <local_ip> <host_ip>\n";
+}
+
+my $local_ip = $ARGV[0];
+my $host_ip  = $ARGV[1];
+
+# ---------------------------------------------------------------
 
 sub dump {
     my ( $device, $message, @rest ) = @_;
-
     print "get message: ", Dumper($message), "\n";
 }
 
 my %args = (
-    addr  => 'X.X.X.X',
+    addr  => $local_ip,
     sport => 47808,
     id    => 42,
 );
@@ -27,7 +37,7 @@ sub approve {
     my ( $device, $message, @rest ) = @_;
     $device->send_approve(
         service_choice => 'ConfirmedCOVNotification',
-        host_ip        => 'X.X.X.X',
+        host_ip        => $host_ip,
         peer_port      => 47808,
         invoke_id      => $message->{invoke_id},
     );
@@ -35,23 +45,27 @@ sub approve {
 
 my $mydevice = BACnet::Device->new(%args);
 
-my %args1 = (    #sub_COV
+# ---------------------------------------------------------------
+# Subscription definitions
+# ---------------------------------------------------------------
+
+my %args1 = (
     obj_type                      => 0,
     obj_inst                      => 1,
     issue_confirmed_notifications => FALSE,
     lifetime_in                   => 100,
-    host_ip                       => 'X.X.X.X',
+    host_ip                       => $host_ip,
     peer_port                     => 47808,
     on_COV                        => \&dump,
     on_response                   => \&dump,
 );
 
-my %args2 = (    #sub_COV
+my %args2 = (
     obj_type                      => 0,
     obj_inst                      => 2,
     issue_confirmed_notifications => TRUE,
     lifetime_in                   => 100,
-    host_ip                       => 'X.X.X.X',
+    host_ip                       => $host_ip,
     peer_port                     => 47808,
     on_COV                        => \&dump,
     on_response                   => \&dump,
@@ -62,18 +76,27 @@ my %args_read_prop = (
     obj_instance         => 2,
     property_identifier  => 85,
     property_array_index => undef,
-    host_ip              => 'X.X.X.X',
+    host_ip              => $host_ip,
     peer_port            => 47808,
     on_response          => \&dump,
 );
-
 
 sub another_sub {
     my ( $device, $message, @rest ) = @_;
     $device->subscribe(%args1);
 }
 
-my ($new_sub, $error) = $mydevice->subscribe(%args1);
+# ---------------------------------------------------------------
+# Perform subscriptions
+# ---------------------------------------------------------------
+
+my ($new_sub,  $error)  = $mydevice->subscribe(%args1);
 my ($new_sub2, $error2) = $mydevice->subscribe(%args2);
+
+# If needed, we could print errors:
+# print "Error: $error\n"   if defined $error;
+# print "Error2: $error2\n" if defined $error2;
+
+# ---------------------------------------------------------------
 
 $mydevice->run;
--- examples/demoSubcsribeUnsubcsribe.pl
+++ examples/demoSubcsribeUnsubcsribe.pl
@@ -8,17 +8,28 @@ use constant TRUE  => 1;
 use constant FALSE => 0;
 
 use BACnet::Device;
-
 use Data::Dumper;
+
+# -------------------------------------------------------------------
+# Command-line arguments
+# -------------------------------------------------------------------
+
+if ( @ARGV < 2 ) {
+    die "Usage: $0 <local_ip> <host_ip>\n";
+}
+
+my $local_ip = $ARGV[0];
+my $host_ip  = $ARGV[1];
+
+# -------------------------------------------------------------------
 
 sub dump {
     my ( $device, $message, @rest ) = @_;
-
     print "get message: ", Dumper($message), "\n";
 }
 
 my %args = (
-    addr  => 'X.X.X.X',
+    addr  => $local_ip,
     sport => 47808,
     id    => 42,
 );
@@ -27,20 +38,21 @@ sub approve {
     my ( $device, $message, @rest ) = @_;
     $device->send_approve(
         service_choice => 'ConfirmedCOVNotification',
-        host_ip        => 'X.X.X.X',
+        host_ip        => $host_ip,
         peer_port      => 47808,
         invoke_id      => $message->{invoke_id},
     );
 }
 
-my %args1 = (    #sub_COV
+# Subscription definition
+my %args1 = (
     obj_type                      => 0,
     obj_inst                      => 1,
     issue_confirmed_notifications => FALSE,
     lifetime_in                   => 10,
-    host_ip                       => 'X.X.X.X',
+    host_ip                       => $host_ip,
     peer_port                     => 47808,
-    on_COV                        => \&point,
+    on_COV                        => \&dump,
     on_response                   => \&dump,
 );
 
@@ -50,6 +62,7 @@ sub another_sub {
 }
 
 my $mydevice = BACnet::Device->new(%args);
+
 my ( $new_sub, $error ) = $mydevice->subscribe(%args1);
 
 sleep(5);
